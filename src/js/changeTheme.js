a262a597b3f3605fd83b62eab094f4a501365b45
Yura <streltsovyurii86@gmal.com>
2020-10-25 10:05:28 +0200

    Добавил фун-ию переключения темы и запись состояния в локалСторадже

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -1,10 +1,19 @@
+import storage from '../js/storage'
 const inputEl = document.querySelector('#theme-switch-toggle');
-
 const bodyEl = document.querySelector('body')
 
-inputEl.addEventListener('change', foo)
+inputEl.addEventListener('change', onSwitchTheme)
 
-function foo() {        
-    bodyEl.classList.toggle('dark-theme', inputEl.checked)
-    bodyEl.classList.toggle('ligth-theme', !inputEl.checked)    
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+function onSwitchTheme() {
+    bodyEl.classList.toggle(Theme.LIGHT, !bodyEl.classList.toggle(Theme.DARK));
+    storage.save('Theme', bodyEl.classList[0]);
 }
+
+const themeChosen = storage.load('Theme');
+inputEl.checked = themeChosen === 'dark-theme' ? true : false;
+bodyEl.classList.add(themeChosen ? themeChosen : Theme.LIGHT);
